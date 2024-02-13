140c13ef5d9e4fba91b2263148d70ad96d5b9f5a
boykethio22 <159900662+boykethio22@users.noreply.github.com>
2024-02-13 22:19:52 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 5;
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function() {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "AKHIRNYA hehe                                     Timakaci udah jadi pacal aku ya, lapyu so bad. Kamu jgn pergi ya awas ae kalo pergi aku pencet nenen nya. Semangat skripsit nya, semoga lancar sampe luluz, slow aku bantu doa brok, maaci yahh udah byk sabar ke aku, moga ga keinget2 lg sm kesalahan ku. :) lapyu ayank ";
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No",
+    "batu banget y",
+    "makin batu lama lama",
+    "punya mata ga",
+    "BENERAN GAPUNYA TERNYATA",
+    "JANGAN PENCET INI MULU ",
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
